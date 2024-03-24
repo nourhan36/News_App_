911a98763851727e16b522f9340af5094d9dbf06
nourhan36 <nourhanibrahim2004@gmail.com>
2024-03-24 20:18:41 +0200

    + drawer

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
 import com.example.newsapp.databinding.ActivityMainBinding
@@ -11,10 +12,37 @@ class MainActivity : AppCompatActivity(), CategoriesFragment.CategoryClickListen
     private lateinit var viewBinding: ActivityMainBinding
     private val newsSourcesFragment = NewsSourcesFragment()
     private val categoriesFragment = CategoriesFragment()
+    private val settingsFragment = SettingsFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBinding.imvMenu.setOnClickListener {
+            viewBinding.root.openDrawer(GravityCompat.START)
+        }
+
+        viewBinding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.categoriesMenuItem -> {
+                    val fragment = categoriesFragment
+                    pushFragment(fragment)
+                    true
+                }
+
+                R.id.settingsMenuItem -> {
+                    val fragment = settingsFragment
+                    pushFragment(fragment)
+                    true
+                }
+
+                else -> {
+                    val fragment = categoriesFragment
+                    pushFragment(fragment)
+                    true
+                }
+            }
+        }
 
         categoriesFragment.categoryClickListener = this
         pushFragment(categoriesFragment)
@@ -31,5 +59,7 @@ class MainActivity : AppCompatActivity(), CategoriesFragment.CategoryClickListen
             .beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+
+        viewBinding.root.closeDrawer(GravityCompat.START)
     }
 }
